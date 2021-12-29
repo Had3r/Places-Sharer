9d72e7646ee92c87d676aa571b8693e084f0a67a
Adrian <adrian.zawadzki@jmr.pl>
2021-12-29 07:51:36 +0100

    feat: add user routes

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const httpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not finf this route", 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
